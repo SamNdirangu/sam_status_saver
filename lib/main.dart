aab4e315707b4d4f4cc128f4dd6ce03e4b000efd
Samuel Ndirangu <sndirangu7@gmail.com>
2023-07-17 05:32:14 +0300

    Update main.dart
    Hot theme fix

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class App extends ConsumerWidget {
       theme: ThemeData(
         primaryColor: colorCustom,
         colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: colorCustom,
           accentColor: colorCustom,
         ),
       ),
